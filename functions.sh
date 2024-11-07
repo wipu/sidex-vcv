66c730c8672de6659a727bd94ba8d72f28a6c614
Ville Oikarinen <ville@oikarinen.org>
2024-11-07 21:28:36 +0200

    fixed broken Rack dep urls
    The antipattern of changing URLs of artifacts, just to move them under
    some "archive" path or something. Or for some even worse reason.

--- functions.sh
+++ functions.sh
@@ -3,6 +3,10 @@
 RACK_COMMIT=652b4449548c4a2ddb9dd6cb955bcf5d352d8031
 FUNDAMENTAL_COMMIT=eb41dd7cdf1112530c1a8059cfa3a99a402944ef
 
+ZLIB_URL=https://zlib.net/fossils/zlib-1.2.11.tar.gz
+PFFFT_VER=fbc405860280
+PFFFT_SHA=03294be29a772ac515d7d5f07bf475b10fe644df132ff56dee4a18338e767533
+
 # TODO use official Rack or even SDK when it contains sysex send:
 RACK=/tmp/Rack-$RACK_COMMIT
 [ -e "$RACK".done ] || {
@@ -10,6 +14,11 @@ RACK=/tmp/Rack-$RACK_COMMIT
     git clone https://github.com/wipu/Rack.git "$RACK"
     cd "$RACK"
     git checkout "$RACK_COMMIT"
+
+    sed -i "s|https://www.zlib.net/zlib-1.2.11.tar.gz|$ZLIB_URL|" dep/Makefile
+    sed -i "s|29e4f76ac53b|$PFFFT_VER|g" dep/Makefile
+    sed -i "s|bb10afba127904a0c6c553fa445082729b7d72373511bda1b12a5be0e03f318a|$PFFFT_SHA|" dep/Makefile
+
     git submodule update --init --recursive
     make -j$(nproc) dep
     cd plugins
